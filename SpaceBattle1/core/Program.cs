1acfc6eb22f21169a144dc613fb8c0a382840280
pabloduke <pd@email.com>
2024-04-01 20:56:50 -0500

    removing superflous code

--- SpaceBattle1/core/Program.cs
+++ SpaceBattle1/core/Program.cs
@@ -1,6 +1,5 @@
 ﻿using NLog;
 using SFML.Graphics;
-using SFML.System;
 using SFML.Window;
 using SpaceBattle1.core;
 using SpaceBattle1.core.display;
@@ -16,24 +15,15 @@ class Program {
 
     static void Main(string[] args) {
         log.Info("Initializing");
-        int width = 1200;
-        int height = 1000;
-        int cellSize = 100;
-        
-        int shipX = 0;
-        int shipY = 100;
-        RenderWindow window = new RenderWindow(new VideoMode((uint)width, (uint)height), "Space Battle");
-        RenderTexture backgroundTexture = new RenderTexture((uint)width, (uint)height);
-        
+
+        RenderWindow window = new RenderWindow(new VideoMode((uint)WIDTH, (uint)HEIGHT), "Space Battle");
         window.SetFramerateLimit(60);
         window.Closed += (sender, e) => ((Window)sender).Close();
         
-
         Texture nebulaTexture = new Texture("C:\\Users\\steph\\RiderProjects\\SpaceBattle1\\SpaceBattle1\\images\\alt_nebula.jpg");
         Texture ship_a_texture = new Texture("C:\\Users\\steph\\RiderProjects\\SpaceBattle1\\SpaceBattle1\\images\\nx01.png");
         Sprite nebulaSprite = new Sprite(nebulaTexture);
         Sprite shipASprite = new Sprite(ship_a_texture);
-        shipASprite.Position = new Vector2f(shipX, shipY);
     
         SpaceShip enterprise = SpaceShip.CreateShip(
             "Enterprise",
@@ -45,8 +35,7 @@ class Program {
         
         Texture ship_b_texture = new Texture("C:\\Users\\steph\\RiderProjects\\SpaceBattle1\\SpaceBattle1\\images\\enemyship.png");
         Sprite shipBSprite = new Sprite(ship_b_texture);
-        shipBSprite.Position = new Vector2f(800, 500);
-
+        
         SpaceShip enemyShip = SpaceShip.CreateShip(
             "Enemy Ship",
             new MediumHull(new Laser(), new Laser()),
@@ -68,13 +57,13 @@ class Program {
        //MAIN GAME LOOP
         while (window.IsOpen) {
             window.DispatchEvents();
-            if (GameContext.getInstance().getLeftButtonClickedInd()) {
+            if (getInstance().getLeftButtonClickedInd()) {
                 Tuple<int, int> clickedCell = new Tuple<int, int>(
-                    GameGridGridResolver.getGridCoor(GameContext.getInstance().MouseClickX, GameContext.getInstance().MouseClickY).Item1,
-                    GameGridGridResolver.getGridCoor(GameContext.getInstance().MouseClickX, GameContext.getInstance().MouseClickY).Item2
+                    GameGridGridResolver.getGridCoor(getInstance().MouseClickX, getInstance().MouseClickY).Item1,
+                    GameGridGridResolver.getGridCoor(getInstance().MouseClickX, getInstance().MouseClickY).Item2
                 );
                 
-                GameContext.getInstance().setLeftButtonClickedInd(false);
+                getInstance().setLeftButtonClickedInd(false);
                 SpriteMover.execute(
                     window,
                     enterprise,
@@ -84,15 +73,15 @@ class Program {
                 );
             }
 
-            if (GameContext.getInstance().Keypress == Keyboard.Key.Num1 || GameContext.getInstance().Keypress == Keyboard.Key.Numpad1) {
+            if (getInstance().Keypress == Keyboard.Key.Num1 || getInstance().Keypress == Keyboard.Key.Numpad1) {
                 log.Info("ATTACK!!!!");
-                GameContext.getInstance().Keypress = Keyboard.Key.Unknown;
+                getInstance().Keypress = Keyboard.Key.Unknown;
             }
         }
     }
     private static void OnMouseButtonPressed(object sender, MouseButtonEventArgs e) {
         if (e.Button == Mouse.Button.Left) {
-            Boolean toggleState = GameContext.getInstance().getLeftButtonClickedInd();
+            Boolean toggleState = getInstance().getLeftButtonClickedInd();
             getInstance().setLeftButtonClickedInd(!toggleState);
             getInstance().MouseClickX = e.X;
             getInstance().MouseClickY = e.Y;
@@ -101,6 +90,6 @@ class Program {
 
     private static void OnKeyPress(object sender, KeyEventArgs e) {
         log.Info($"You pressed {e.Code}");
-        GameContext.getInstance().Keypress = e.Code;
+        getInstance().Keypress = e.Code;
     }
 }
