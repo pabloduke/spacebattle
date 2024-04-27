10274bcd6f581004e3ecc2a7f828c019635c582e
pabloduke <pd@email.com>
2024-04-26 21:59:17 -0500

    remove unused param

--- SpaceBattle1/display/MainMenuDrawer.cs
+++ SpaceBattle1/display/MainMenuDrawer.cs
@@ -16,19 +16,19 @@ public class MainMenuDrawer {
         window.Draw(mainMenu);
         Font menuFont = GlobalGameContext.getInstance().MenuFont;
         Text attackText = new Text("2. Attack", menuFont);
-        drawMoveMenuText(window, menuFont, mainMenu);
-        drawAttackMenuText(window, menuFont, mainMenu);
+        drawMoveMenuText(window, menuFont);
+        drawAttackMenuText(window, menuFont);
         drawShipInfo(window, menuFont);
     }
 
-    private static void drawMoveMenuText(RenderWindow window, Font font, RectangleShape mainMenu) {
+    private static void drawMoveMenuText(RenderWindow window, Font font) {
         Text text = new Text("1. Move", font);
         text.FillColor = Color.Green;
         text.Position = new Vector2f(10, 810);
         window.Draw(text);
     }
     
-    private static void drawAttackMenuText(RenderWindow window, Font font, RectangleShape mainMenu) {
+    private static void drawAttackMenuText(RenderWindow window, Font font) {
         Text text = new Text("2. Attack", font);
         text.FillColor = Color.Green;
         text.Position = new Vector2f(10, 850);
@@ -48,6 +48,5 @@ public class MainMenuDrawer {
                 window.Draw(text);
             }  
         }
-        
     }
 }
